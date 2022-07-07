56bf898831454a36cab7ba2e0b889a573206043f
helloitsian <15301476+helloitsian@users.noreply.github.com>
2022-07-07 00:16:49 -0500

    fixed bug with config

--- bin/index.js
+++ bin/index.js
@@ -1,61 +1,59 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const path = require("path");
-const yargs = require('yargs');
-const { build } = require('../lib/index');
+const yargs = require("yargs");
+const { build } = require("../lib/index");
 
 const argv = yargs(process.argv)
-    .option("entry", {
-      alias: "e",
-      describe: "Entry point",
-    })
-    .option("outdir", {
-      alias: "o",
-      describe: "Output directory",
-    })
-    .option("html", {
-      describe: "Html Output file path",
-    })
-    .option("css", {
-      describe: "Css Output file path",
-    })
-    .option("nondefered", {
-      describe: "Non-Defered JavaScript Output file path",
-    })
-    .option("defered", {
-      describe: "Defered JavaScript Output file path",
-    })
-    .option("config", {
-      alias: "c",
-      describe: "Path to config file"
-    })
-    .help().argv;
+  .option("entry", {
+    alias: "e",
+    describe: "Entry point",
+    default: "./index.html",
+  })
+  .option("dir", {
+    alias: "d",
+    describe: "Output Directory",
+    default: "dist",
+  })
+  .option("html", {
+    describe: "Html Output file path",
+    default: "index.html",
+  })
+  .option("css", {
+    describe: "Css Output file path",
+    default: "style.css",
+  })
+  .option("immediate", {
+    describe: "Non-Defered JavaScript Output file path",
+    default: "bundle.js",
+  })
+  .option("defered", {
+    describe: "Defered JavaScript Output file path",
+    default: "defered.bundle.js",
+  })
+  .option("config", {
+    alias: "c",
+    describe: "Path to config file",
+    default: "./pallet.config.js",
+  })
+  .help().argv;
 
-if (argv.config) {
-  const config = fs.readFileSync(path.join(__dirname, '/', argv.config), "utf8");
-  const parsedConfig = JSON.parse(config);
-  build(parsedConfig);
-} else if (argv.entry) {
-
-  const htmlArg = argv.html ? argv.html.replace(argv.outdir, "") : null;
-  const cssArg = argv.css ? argv.css.replace(argv.outdir, "") : null;
-  const nonDeferedArg = argv.nondefered ? argv.nondefered.replace(argv.outdir, "") : null;
-  const deferedArg = argv.defered ? argv.defered.replace(argv.outdir, "") : null;
-
+if (fs.existsSync(path.join(process.cwd(), argv.config))) {
+  const config = require(path.join(process.cwd(), argv.config));
+  build(config);
+} else {
   const outputConfig = {
-    dir: argv.outdir,
+    dir: argv.dir,
     html: argv.html,
     css: argv.css,
     js: {
-      nonDefered: argv.nondefered,
+      immediate: argv.immediate,
       defered: argv.defered,
-    }
-  }
-
+    },
+  };
   build({
     entry: argv.entry,
+    shouldOutput: argv.output === "true" ? true : false,
     output: outputConfig,
   });
-} else {
-  build();
 }
